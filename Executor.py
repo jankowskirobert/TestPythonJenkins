9e5f31cee842fefe4d72b1df437520ab0b8e76e6
jankowskirobert <jankowski.r@hotmail.com>
2015-05-13 22:23:20 +0200

    Test 1

--- Executor.py
+++ Executor.py
@@ -1,6 +1,8 @@
 __author__ = 'rob'
 
 def fivePrint():
+    print("HEHEH 5")
     return 5
 def sixPrint():
+    print("HEHEH 6")
     return 6
--- TestExecutor.py
+++ TestExecutor.py
@@ -3,6 +3,8 @@ import Executor
 import pytest
 
 def test_five():
+    print("NIEE!")
     return Executor.fivePrint() == 5
 def test_six():
+    print("DUPA!")
     return Executor.sixPrint() == 9
